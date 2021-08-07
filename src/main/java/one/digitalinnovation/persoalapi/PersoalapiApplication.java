958cbe82efa33759914240fde14e570bf4e0e2b5
flashlan <flash_lan@hotmail.com>
2021-08-07 02:41:29 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/persoalapi/PersoalapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.persoalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoalapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersoalapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/persoalapi/PersoalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.persoalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
